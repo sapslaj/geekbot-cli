0f2d1391a698b87c4c34d189bbee083da14dc754
Justin Roberson <justin.roberson@tackle.io>
2022-03-07 10:44:00 -0500

    Error out if no steps have been given

--- cmd/report.go
+++ cmd/report.go
@@ -24,14 +24,18 @@ var reportCmd = &cobra.Command{
 			fmt.Fprintln(os.Stderr, "Uh you can't have --quick and --interactive at the same time! Pick one or none!")
 			os.Exit(1)
 		}
-		geekbot := geekbotclient.NewGeekbotClient(&geekbotclient.GeekbotClient{})
-		standup := flows.SelectStandup(geekbot, standupName)
 		if quickMode {
+			geekbot, standup := preflow()
 			flows.RunQuickFlow(geekbot, standup)
 		} else if interactiveMode {
 			fmt.Fprintln(os.Stderr, "haven't build interactive mode yet sorry")
 			os.Exit(501)
 		} else {
+			if len(args) == 0 {
+				fmt.Fprintln(os.Stderr, "Provide report commands or use `-q` or `-i`. See `-h` for help.")
+				os.Exit(1)
+			}
+			geekbot, standup := preflow()
 			flows.RunSteppedFlow(geekbot, standup, args)
 		}
 	},
@@ -43,4 +47,10 @@ func init() {
 	reportCmd.Flags().StringVarP(&standupName, "standup", "s", "", "Standup name to report")
 	reportCmd.Flags().BoolVarP(&quickMode, "quick", "q", false, "Create, open, edit, send all in one action")
 	reportCmd.Flags().BoolVarP(&interactiveMode, "interactive", "i", false, "Semi-guided standup process")
+}
+
+func preflow() (*geekbotclient.GeekbotClient, *geekbotclient.Standup) {
+	geekbot := geekbotclient.NewGeekbotClient(&geekbotclient.GeekbotClient{})
+	standup := flows.SelectStandup(geekbot, standupName)
+	return geekbot, standup
 }
